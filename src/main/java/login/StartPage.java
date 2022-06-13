cd96625e411da74b05aa6da515e7a1428703c396
Caro Feldermann <feldermann@heptacom.de>
2022-06-13 09:09:21 +0200

    Login - add preliminary login feature and welcome page

---
+++ src/main/java/login/StartPage.java
@@ -0,0 +1,12 @@
+package login;
+
+import javax.swing.*;
+
+public class StartPage extends JFrame {
+    StartPage()
+    {
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("Willkommen");
+        setSize(400, 200);
+    }
+}
